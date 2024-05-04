2a56ec8d5119e1237369d46941dd7795142bf6fd
Inesa Soloviova <inesa200088@gmail.com>
2024-05-04 13:00:29 +0300

    add cycles

--- js/lesson1.js
+++ js/lesson1.js
@@ -72,33 +72,108 @@ const lastNameLength = lastName.length; */
 // Запитай користувача у модальному вікні, який з напоїв він бажає придбати - "Кава", "Чай" чи "Сік",
 // та залежно від виду напою оголоси суму досплати.Використовувати switch.
 
-const drink = prompt("Який напій бажаєте?").toLowerCase();
+// const drink = prompt("Який напій бажаєте?").toLowerCase();
 
-function orderDrink(type) {
-  //   let price = 0;
+// function orderDrink(type) {
+//   //   let price = 0;
 
-  switch (type) {
-    case "кава":
-      return "Сума до сплати 40 грн";
-    //   price = 40;
-    //   break;
+//   switch (type) {
+//     case "кава":
+//       return "Сума до сплати 40 грн";
+//     //   price = 40;
+//     //   break;
 
-    case "чай":
-      return "Сума до сплати 25 грн";
-    //   price = 25;
-    //   break;
+//     case "чай":
+//       return "Сума до сплати 25 грн";
+//     //   price = 25;
+//     //   break;
 
-    case "сік":
-      return "Сума до сплати 50 грн";
-    //   price = 50;
-    //   break;
+//     case "сік":
+//       return "Сума до сплати 50 грн";
+//     //   price = 50;
+//     //   break;
 
-    default:
-      return "Напій відсутній";
-    //   break;
-  }
+//     default:
+//       return "Напій відсутній";
+//     //   break;
+//   }
 
-  //   alert(`Сума до сплати ${price} грн`);
-}
+//   //   alert(`Сума до сплати ${price} грн`);
+// }
 
-alert(orderDrink(drink));
+// alert(orderDrink(drink));
+
+// * Цикли: while, do while, for
+
+//TODO:============task-01=========================
+// скористаємося циклом while та виведіть у консоль числа від 0 до 50
+
+// let i = 0;
+// while (i <= 50) {
+//   console.log(i);
+//   i += 1;
+// }
+
+// for (let i = 0; i <= 50; i += 1) {
+//   console.log(i);
+// }
+
+//TODO:============task-02=========================
+// Напишіть цикл, який пропонує ввести
+// число більше 100 через prompt.
+// Якщо відвідувач ввів інше число - попросити
+// ввести ще раз і так далі.
+// Цикл має питати число, поки відвідувач не
+// введе число більше 100, або натисне кнопку
+// скасування в prompt
+
+// let num = Number(prompt("Введіть число більше 100"));
+// while (num <= 100) {
+//   num = Number(prompt("Введіть число більше 100"));
+// }
+
+//TODO:============task-03=========================
+//  Виведіть методом console.log() зірочки від 1 до 7 штук у вигляді трикутника таким чино
+// *
+// **
+// ***
+// ****
+// *****
+
+// let start = "*";
+// for (let i = 1; i <= 7; i += 1) {
+//   console.log(start);
+//   start += "*";
+// }
+
+//TODO:============task-04=========================
+// Порахуйте суму чисел від 12 до 27
+
+// const num1 = 12;
+// const num2 = 27;
+// let totalSum = 0;
+// for (let i = num1; i <= num2; i += 1) {
+//   if (i % 2 === 0) {
+//     totalSum += i;
+//   }
+// }
+// console.log(totalSum);
+
+//TODO:============task-04=========================
+// Знайдіть число 49  у проміжку чисел від 1 до 100 та вийдіть з циклу.
+
+// for (let i = 1; i <= 100; i += 1) {
+//   console.log(i);
+//   if (i === 49) {
+//     console.log(i);
+//     break;
+//   }
+// }
+
+// for (let i = 1; i <= 100; i += 1) {
+//   console.log(i);
+//   if (i !== 49) {
+//     continue;
+//   }
+//   console.log(i);
+// }
