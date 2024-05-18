a872dd7dd2525f1a86acd380b46aa1647f124fa0
Yevhenii-Tsariuk <care64678@gmail.com>
2024-05-18 13:19:14 +0300

    Task 7

--- js/lesson2.js
+++ js/lesson2.js
@@ -162,3 +162,42 @@
 // console.log(courses); // ["HTML", "CSS", "TypeScript", "Node.js"]
 // removeCourse("Ruby"); // Course Ruby not found
 // updateCourse("Python", "Django"); // Course Python not found
+
+
+// TASK-7
+
+
+// Угруповання об'єктів студентів по курсам
+// Створіть масив об'єктів, що являють собою різних студентів. Кожен об'єкт повинен містити властивості ім'я, вік та курс.
+// Використовуйте цикл for...of, щоб згрупувати студентів за курсами.
+// Результатом буде об'єкт, де ключами будуть назви курсів, а значення – масиви об'єктів студентів, що належать до відповідного курсу.
+// Очікуванний результат:
+// const groupedStudents = {
+//   Java: [
+//     { name: "Алиса", age: 20, course: "Java" },
+//     { name: "Карл", age: 21, course: "Java" },
+//   ],
+//   Python: [{ name: "Боб", age: 22, course: "Python" }],
+//   JavaScript: [{ name: "Джон", age: 23, course: "JavaScript" }],
+// };
+  const students = [
+    { name: "Алиса", age: 20, course: "Java" },
+    { name: "Боб", age: 22, course: "Python" },
+    { name: "Карл", age: 21, course: "Java" },
+    { name: "Джон", age: 23, course: "JavaScript" },
+  ];
+
+
+function groupStudentsByCourse(students){
+const groupedStudents = {};
+for(const student of students){
+     
+     if(!groupedStudents[student.course]){
+          groupedStudents[student.course] = [];
+     }
+     groupedStudents[student.course].push(student);
+     // console.log(groupedStudents[student.course]);
+}
+return groupedStudents;
+}
+console.log(groupStudentsByCourse(students));
