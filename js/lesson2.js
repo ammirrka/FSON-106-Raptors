bf62f5f961d3839378a103f39193c6cea616e13a
Yevhenii-Tsariuk <care64678@gmail.com>
2024-05-18 11:21:07 +0300

    done third task

--- js/lesson2.js
+++ js/lesson2.js
@@ -33,3 +33,28 @@
 //     return longestWord;
 // }
 // console.log(findLongestWord(string));
+
+//TODO:=======================
+//Напишіть ф-цію calcTotalPrice(stones, stonesName),
+//яка приймає масив об'єктів і
+//Рядок з назвою каменю.
+//Функція вважає, що м повертає загальну вартість каменів
+//з ​​таким ім'ям, ціною та кількістю з об'єкта
+// const stones = [
+//   { name: "Смарагд", price: 1300, quantity: 4 },
+//   { name: "Діамант", price: 2700, quantity: 6 },
+//   { name: "Сапфір", price: 400, quantity: 7 },
+//   { name: "Щебінь", price: 150, quantity: 100 },
+// ];
+
+// function calcTotalPrice(stones, stonesName) {
+//   for (const stone of stones) {
+//     if (stonesName === stone.name) {
+//      console.log(stone);
+//       const totalPrice = stone.price * stone.quantity;
+//       return `${stone.name} will cost ${totalPrice}`;
+//     }
+//   }
+ 
+// }
+// console.log(calcTotalPrice(stones, "Діамант"));
